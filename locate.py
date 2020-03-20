1a37fb158f9b7bfd7d305059a25c5ee27e90c04c
bitsTyrant <acm_liyuan@163.com>
2020-03-20 09:33:55 +0800

    fix path problem in locate.get_root()

--- locate.py
+++ locate.py
@@ -3,4 +3,5 @@ import os
 # 获取被分析文件的源文件的根目录绝对路径
 def get_root():
     path = os.getcwd()
-    return os.path.join(path, 'MordorLogAnalysis', 'log_pod')
+    print(path)
+    return os.path.join(path, 'log_pod')
